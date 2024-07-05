1ef394f37bebd9f9c0f300b9c0671a6e7b76acf1
jung-sungwon <pompous89@gmail.com>
2024-07-05 21:32:32 +0900

    메서드

--- 123.py
+++ 123.py
@@ -12,9 +12,19 @@ class Member:
         print(f"Name:{self.name}  ID:{self.username}")
 
 
-
 class Post():
     def __init__(self, title, content, author):
         self.title = title
         self.content = content
         self.author = author
+
+
+members = []
+
+member1 = Member("정성원", "ai7", "123123")
+member2 = Member("장성원", "ai8", "231231")
+member3 = Member("종성원", "ai9", "312312")
+
+members.append(member1)
+members.append(member2)
+members.append(member3)
