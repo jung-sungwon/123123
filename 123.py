a1ffbc30dc493b05828139880afebe12aa74e6e0
chanloper <dbstjddl0396@naver.com>
2024-07-05 21:22:46 +0900

    class

--- 123.py
+++ 123.py
@@ -1,3 +1,16 @@
 print(1)
 
-print("hello")
+
+class Member:
+    def __init__(self, name, username, password):
+        # 생성자 구현
+        self.name = name
+        self.username = username
+        self.password = password
+
+
+class Post():
+    def __init__(self, title, content, author):
+        self.title = title
+        self.content = content
+        self.author = author
